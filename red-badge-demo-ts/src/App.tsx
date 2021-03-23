93898ddb56ec315c92b7dda867abfe8e02c2ec9a
gingeralford <Ginger@Gingers-MacBook-Pro.local>
2021-03-23 19:46:54 -0400

    props and mapping practice

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import PropsExample from '../src/components/PropsExample'
+import PropsMapping from '../src/components/PropsMapping'
 
 function App() {
-  return <div></div>;
+  const visitedPlaces = ['Wales', 'Croatia', 'Mexico', 'Germany', 'Austria', 'Scotland']
+  return <div>
+    <PropsExample name="Tom" business="MySpace"/>
+    <PropsMapping visited={visitedPlaces}/>
+  </div>;
 }
 
 export default App;
