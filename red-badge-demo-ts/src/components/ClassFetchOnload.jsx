ea192bec8abac607c5fdc99b25178e19e5996953
gingeralford <Ginger@Gingers-MacBook-Pro.local>
2021-03-25 20:51:36 -0400

    state class fetch challenge

--- red-badge-demo-ts/src/components/ClassFetchOnload.jsx
+++ red-badge-demo-ts/src/components/ClassFetchOnload.jsx
@@ -1,1 +1,60 @@
-// class component for conversion
+import React, { Component } from 'react';
+import { Container, Media } from "reactstrap";
+
+class ClassFetchOnload extends Component {
+    constructor(props){
+        super(props);
+        this.state = { blogPostData : [] };
+        
+    };
+
+    fetchBlogData() {
+        const url = "https://jsonplaceholder.typicode.com/comments?postId=1";
+        fetch(url)
+          .then((res) => res.json())
+          .then((json) => this.setState({ blogPostData : json }));
+    };
+
+    componentDidMount(){
+        this.fetchBlogData();
+    };
+
+    displayBlogPosts() {
+        return this.state.blogPostData.map((data) => (
+          <Media>
+            <Media left href="#">
+              <Media
+                object
+                src="https://picsum.photos/60"
+                alt="Generic placeholder image"
+                style={{ marginRight: "10px" }}
+              />
+            </Media>
+            <Media body>
+              <Media heading>{data.name.toUpperCase()}</Media>
+              {data.body}.
+              <p>
+                Email: <a href={`mailto:${data.email}`}>{data.email}</a>
+              </p>
+            </Media>
+          </Media>
+        ));
+      }
+
+    //do I need this? Will calling it below work by itself?
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.blogPostData !== this.state.blogPostData) {
+            this.displayBlogPosts();
+        };
+    } ;
+
+    render(){
+        return (
+            <div>Hello from ClassFetchOnload
+                {this.displayBlogPosts()}
+            </div>
+        )
+    };
+};
+
+export default ClassFetchOnload;
