f311dc52dfd8471de06d01077e61097f6b30a69c
gingeralford <Ginger@Gingers-MacBook-Pro.local>
2021-03-30 17:43:32 -0400

    diceproject

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="container">
-      <p>Hi from App.tsx</p>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ red-badge-demo-ts/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export interface FooterProps {
+    
+}
+ 
+const Footer: React.SFC<FooterProps> = () => {
+    return ( <div>Let them Roll 🎲 🎲  </div> );
+}
+ 
+export default Footer;
